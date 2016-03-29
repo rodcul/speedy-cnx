b0bbc9737ab7f166453cec61fdc2ae6066c0c110
Rodney Cullen <rodcul@gmail.com>
2016-03-29 20:28:07 +0700

    Added hostname to speedtests model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329102102) do
+ActiveRecord::Schema.define(version: 20160329132725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160329102102) do
     t.decimal  "download"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "hostname"
   end
 
 end
